6ac8124999e2f1f35993ef9de4d8ff475c299151
EgilLapin <egilslapins@gmail.com>
2022-01-24 06:02:03 +0200

    initial

---
+++ src/main/java/com/motogram/motogram/MotogramApplication.java
@@ -0,0 +1,13 @@
+package com.motogram.motogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motogram/motogram/MotogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motogram.motogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
