0acf9c18d6c5d2fc30faea80e8fd6814a56fb739
EgilLapin <egilslapins@gmail.com>
2022-01-24 07:36:08 +0200

    Add Password encryption

--- src/test/java/com/motogram/motogram/UserControllerTest.java
+++ src/test/java/com/motogram/motogram/UserControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -62,5 +64,14 @@ public class UserControllerTest {
         User user = createValidUser();
         ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
         assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase(){
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_0_USERS,user,Object.class);
+        List<User> users = userRepository.findAll();
+        User inDB = users.get(0);
+        assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
     }
 }
